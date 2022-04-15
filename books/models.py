2de0a0a8236c92bd84aa0fbe14c8f784746d9880
shahdin-sh <99738074+shahdin-sh@users.noreply.github.com>
2022-04-15 17:31:25 +0430

    add books_author in models.py

--- books/models.py
+++ books/models.py
@@ -1,6 +1,6 @@
 from django.urls import reverse
-
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
@@ -10,6 +10,7 @@ class Book(models.Model):
     description = models.TextField()
     publisher = models.CharField(max_length=50, null=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    books_author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
