896a69e4883630855b32fd336240bc6f6d38567a
shahdin-sh <99738074+shahdin-sh@users.noreply.github.com>
2022-04-16 16:17:04 +0430

    add login_required to our views

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', books_list_view, name='books_list'),
     path('<int:pk>/', books_detail_view, name='books_detail'),
-    path('create/', BookCreateView.as_view(), name='create_books'),
-    path('<int:pk>/update', BookUpdateView.as_view(), name='update_books'),
-    path('<int:pk>/delete', BookDeleteView.as_view(), name='delete_books')
+    path('create/', login_required(BookCreateView.as_view()), name='create_books'),
+    path('<int:pk>/update', login_required(BookUpdateView.as_view()), name='update_books'),
+    path('<int:pk>/delete', login_required(BookDeleteView.as_view()), name='delete_books')
 ]
--- books/views.py
+++ books/views.py
@@ -3,13 +3,17 @@ from .models import Book
 from django.shortcuts import render
 from .forms import BookForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 
 
+@login_required
 def books_list_view(request):
     books = Book.objects.all()
     return render(request, 'books/books_list_view.html', {'books': books})
 
 
+@login_required
 def books_detail_view(request, pk):
     books_detail = Book.objects.get(pk=pk)
     return render(request, 'books/books_detail_view.html', {'books_detail': books_detail})
