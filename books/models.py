1175f82474c8adc76689053bc33dfb95cf5a0146
shahdin-sh <99738074+shahdin-sh@users.noreply.github.com>
2022-04-17 15:57:30 +0430

    add verbos name in database at models.py

--- books/models.py
+++ books/models.py
@@ -4,13 +4,13 @@ from django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=50)
-    translator = models.CharField(max_length=50)
-    description = models.TextField()
-    publisher = models.CharField(max_length=50, null=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    books_author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=100, verbose_name='عنوان')
+    author = models.CharField(max_length=50, verbose_name='نویسنده')
+    translator = models.CharField(max_length=50, verbose_name='مترجم')
+    description = models.TextField(verbose_name='توضیحات کتاب')
+    publisher = models.CharField(max_length=50, null=True, verbose_name='انتشارات')
+    price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='قیمت')
+    books_author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True, verbose_name='کاربر')
 
     def __str__(self):
         return self.title
