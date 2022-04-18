a9ed4eee04f41cb1a7a8815dc8440381496a1716
shahdin-sh <99738074+shahdin-sh@users.noreply.github.com>
2022-04-18 18:54:24 +0430

    access user to upload photo for book's cover

--- books/forms.py
+++ books/forms.py
@@ -5,4 +5,4 @@ from .models import Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['title', 'author', 'translator', 'description', 'publisher', 'price', 'books_author']
+        fields = ['title', 'author', 'translator', 'description', 'publisher', 'price', 'books_author', 'book_cover']
