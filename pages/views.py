3702195fa03b638cc47d80dd539779fd2fe38dda
shahdin-sh <99738074+shahdin-sh@users.noreply.github.com>
2022-04-25 17:56:57 +0430

    add homepage content

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from books.models import Book
+from django.core.paginator import Paginator
 
 
 def home_page_view(request):
-    return render(request, 'pages/home.html')
+    books = Book.objects.all()
+    paginator = Paginator(books, 4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'pages/home.html', {'books': page_obj})
